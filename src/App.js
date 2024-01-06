55bc8184886e1c283127cf89c7bcd0f6029d24a2
Walter Tay Ann Lee <40448679+watate@users.noreply.github.com>
2024-01-06 14:50:28 +0800

    Feature: You are at move X

--- src/App.js
+++ src/App.js
@@ -67,7 +67,9 @@ export default function Game() {
 
   const moves = history.map((squares, move) => {
     let description;
-    if (move > 0) {
+    if (move === currentMove) {
+      description = "You are at move #" + move;
+    } else if (move > 0) {
       description = "Go to move #" + move;
     } else {
       description = "Go to game start";
